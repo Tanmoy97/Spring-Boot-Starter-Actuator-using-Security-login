88a37dcb1fa931acdeb2479eba5231494ceaf81f
TANMOY KHATUA <tanmoykhatua97@gmail.com>
2021-05-22 21:25:32 +0530

    First commit

---
+++ src/test/java/com/collapp/springactuator/SpringActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collapp.springactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
